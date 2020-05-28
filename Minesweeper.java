8208072b1d63d24bb331b1c702253c75932edefb
Ruixin (Ray) Yang <42333187+99starman@users.noreply.github.com>
2020-05-28 00:18:47 -0700

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,3 +1,9 @@
+// Minesweeper is a 1960s era video game played on an m-by-n grid of cells. The goal is to deduce 
+//             which cells contain hidden mines using clues about the number of mines in neighboring cells. 
+// Write a program Minesweeper.java that takes three integer command-line arguments m, n, and k 
+//       and prints an m-by-n grid of cells with k mines, using asterisks for mines and integers 
+//       for the neighboring mine counts (with two space characters between each cell)
+
 public class Minesweeper {
     public static void main(String[] args) {
         int m = Integer.parseInt(args[0]);
